bd4ab034bd7d021e0625a491d38677d5e2cf5939
Pasi Manninen <pasi@ptm.fi>
2015-09-21 17:08:44 +0300

    Initial commit

---
+++ app/src/main/java/fi/ptm/imageswitcherexample/MainActivity.java
@@ -0,0 +1,110 @@
+package fi.ptm.imageswitcherexample;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Created by PTM on 21/09/15.
+ */
+public class MainActivity extends Activity {
+
+    private ImageView imageView;
+    private TextView textView;
+    private ProgressBar progressBar;
+    private final String PATH = "http://ptm.fi/android/";
+    private String[] images = {"image1.jpg","image2.jpg","image3.jpg"};
+    private int imageIndex;
+    private DownloadImageTask task;
+    private float x1,x2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // get views
+        imageView = (ImageView)findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.textView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        // start showing images
+        imageIndex = 0;
+        showImage();
+    }
+
+    // method gets called when user performs any touch event on screen
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                if (x1 < x2) { // left to right -> previous
+                    imageIndex--;
+                    if (imageIndex < 0) imageIndex = images.length-1;
+                } else { // right to left -> next
+                    imageIndex++;
+                    if (imageIndex > (images.length-1)) imageIndex = 0;
+                }
+                showImage();
+                break;
+        }
+        return false;
+    }
+
+    public void showImage() {
+        // create a new AsyncTask object
+        task = new DownloadImageTask();
+        // start asynctask
+        task.execute(PATH + images[imageIndex]);
+    }
+
+    // asynctask class
+    private class DownloadImageTask extends AsyncTask<String,Void,Bitmap> {
+
+        // this is done in UI thread, nothing this time
+        @Override
+        protected void onPreExecute() {
+            // show loading progress bar
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        // this is background thread, load image and pass it to onPostExecute
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            URL imageUrl;
+            Bitmap bitmap = null;
+            try {
+                imageUrl = new URL(urls[0]);
+                InputStream in = imageUrl.openStream();
+                bitmap = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("<<LOADIMAGE>>", e.getMessage());
+            }
+            return bitmap;
+        }
+
+        // this is done in UI thread
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView.setImageBitmap(bitmap);
+            textView.setText("Image " + (imageIndex + 1) + "/" + images.length);
+            // hide loading progress bar
+            progressBar.setVisibility(View.INVISIBLE);
+
+        }
+    }
+
+
+}
